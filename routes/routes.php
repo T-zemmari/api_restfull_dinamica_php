894bae151f93f022fbb3361abac758880a48b9f8
Tarek <tarek@tedybusiness.com>
2022-06-14 12:21:01 +0200

    archivo connection creado , configurando la base de datos y la conexion a la misma

--- routes/routes.php
+++ routes/routes.php
@@ -1,30 +1,61 @@
 <?php
 
-// echo '<pre>'; print_r($_SERVER['REQUEST_URI']); echo '<pre>';
-// return;
 
 $array_routes = explode('/', $_SERVER['REQUEST_URI']);
 $array_routes = array_filter($array_routes);
 $json = [];
 
+//##########################################################//
+//#### Si no se hace ningua peticion a  la api ############//
+//#########################################################//
+
 if (empty($array_routes)) {
     $json = [
         'status' => 404,
-        'result' => "no hay informacion"
+        'result' => "no hay peticion"
     ];
-    echo json_encode($json);
-} else {
-    $json = [
-        'status' => 200,
-        'result' => 'hola'
-    ];
-    echo json_encode($json);
-}
-// echo '<pre>';
-// print_r($array_routes);
-// echo '<pre>';
-// return;
+    echo json_encode($json, http_response_code($json['status']));
+    return;
+} 
 
+//##########################################################//
+//####      Si se hace una peticion a la api   ############//
+//#########################################################//
+
+if(count($array_routes) == 1 && isset($_SERVER['REQUEST_METHOD'])){
+    echo '<pre>'; print_r($_SERVER['REQUEST_METHOD']); echo '</pre>';
+
+    if($_SERVER['REQUEST_METHOD'] == "GET"){
+        $json = [
+            'status' => 200,
+            'result' => "Solicitud GET"
+        ];
+        echo json_encode($json, http_response_code($json['status']));
+    }
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $json = [
+            'status' => 200,
+            'result' => "Solicitud POST"
+        ];
+        echo json_encode($json, http_response_code($json['status']));
+    }
+    if($_SERVER['REQUEST_METHOD'] == "PUT"){
+        $json = [
+            'status' => 200,
+            'result' => "Solicitud PUT"
+        ];
+        echo json_encode($json, http_response_code($json['status']));
+    }
+
+    if($_SERVER['REQUEST_METHOD'] == "DELETE"){
+        $json = [
+            'status' => 200,
+            'result' => "Solicitud DELETE"
+        ];
+        echo json_encode($json, http_response_code($json['status']));
+    }
+
+}
 
 
 return;
