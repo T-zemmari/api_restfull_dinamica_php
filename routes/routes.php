516c7195975423f61beb4bfaf15fa0fb794dbbc1
Tarek <tarek@tedybusiness.com>
2022-06-14 11:59:25 +0200

    obteniendo datos de la url, y enviando una respuesta segun la variable ¨_routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,7 +1,30 @@
 <?php
 
-$json = [
-    'status' => 200,
-    'result' => 'hola'
-];
-echo json_encode($json);
+// echo '<pre>'; print_r($_SERVER['REQUEST_URI']); echo '<pre>';
+// return;
+
+$array_routes = explode('/', $_SERVER['REQUEST_URI']);
+$array_routes = array_filter($array_routes);
+$json = [];
+
+if (empty($array_routes)) {
+    $json = [
+        'status' => 404,
+        'result' => "no hay informacion"
+    ];
+    echo json_encode($json);
+} else {
+    $json = [
+        'status' => 200,
+        'result' => 'hola'
+    ];
+    echo json_encode($json);
+}
+// echo '<pre>';
+// print_r($array_routes);
+// echo '<pre>';
+// return;
+
+
+
+return;
