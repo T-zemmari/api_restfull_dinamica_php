38491044fa48c6dc15e5dd710fb86dccdce5fb2a
Tarek <tarek@tedybusiness.com>
2022-06-14 11:39:35 +0200

    generado de errores

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+//##########################################################//
+//############### MOSTRAR ERRORES #########################//
+//#########################################################//
+
+ini_set('display_errors',1);
+ini_set('log_errors',1);
+ini_set('error_log',"C:/xampp\htdocs\Proyectos\React\Api-restfull-dinamica-php/php_error_log");
+
+
+
+//##########################################################//
+//################## Controlador ###########################//
+//#########################################################//
+
 require_once "controllers/routes-controller.php";
 
 $index = new RoutersController();
