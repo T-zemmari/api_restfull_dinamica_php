f060e2fbaa8f5f98629b57513b1dd89c7a142bf3
Tarek <tarek@tedybusiness.com>
2022-07-08 16:19:01 +0200

    base de datos actualizada

--- database/api_restful (3).sql
+++ database/api_restful (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-07-2022 a las 12:30:43
+-- Tiempo de generación: 08-07-2022 a las 16:18:30
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.0.13
 
@@ -108,6 +108,8 @@ CREATE TABLE `users` (
   `token_user` text DEFAULT NULL,
   `token_exp_user` text DEFAULT NULL,
   `id_experience_user` int(11) DEFAULT NULL,
+  `publi_check_user` tinyint(1) DEFAULT NULL,
+  `remember_me_user` tinyint(1) DEFAULT NULL,
   `id_education_user` int(11) DEFAULT NULL,
   `date_create_user` datetime DEFAULT current_timestamp(),
   `date_update_user` datetime DEFAULT current_timestamp()
@@ -117,17 +119,8 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id_user`, `name_user`, `lastname_user`, `email_user`, `password_user`, `metodo_registro_user`, `phone_user`, `adress_user`, `cp_user`, `city_user`, `country_user`, `nif_user`, `token_user`, `token_exp_user`, `id_experience_user`, `id_education_user`, `date_create_user`, `date_update_user`) VALUES
-(2, 'Tarek 2', 'Moreno lopez', '', '123456789', NULL, '', '', '', '', '', '', NULL, NULL, 1, NULL, '2022-06-15 11:11:46', '2022-06-15 11:11:46'),
-(3, 'Maria', 'La vega', 'maia@gmail.com', '123456789', NULL, '666666666', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, '2022-06-15 16:38:30', '2022-06-15 16:38:30'),
-(4, 'Fran', 'Mario dario', 'mario@gmail.com', '123456789', NULL, '666666666', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-06-15 16:39:32', '2022-06-15 16:39:32'),
-(6, 'Paco', 'Lopez Flores', 'lopez@gmail.com', '123456789', NULL, '666666666', 'calle la paz mundial 21', '46001', 'Valencia', 'España', '23669988L', NULL, NULL, NULL, NULL, '2022-06-22 13:44:10', '2022-06-22 13:44:10'),
-(7, 'Paco', 'Lopez Flores', 'lopez@gmail.com', '123456789', NULL, '666666666', 'calle la paz mundial 21', '46001', 'Valencia', 'España', '23669988L', NULL, NULL, NULL, NULL, '2022-06-22 13:50:29', '2022-06-22 13:50:29'),
-(8, 'Paco', 'Lopez Flores', 'lopez@gmail.com', '123456789', NULL, '666666666', 'calle la paz mundial 21', '46001', 'Valencia', 'España', '23669988L', NULL, NULL, NULL, NULL, '2022-06-30 15:18:54', '2022-06-30 15:18:54'),
-(9, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2022-06-30 16:43:22', '2022-06-30 16:43:22'),
-(10, 'Paco', 'Lopez Flores', 'lopez@gmail.com', '123456789', NULL, '666666666', 'calle la paz mundial 21', '46001', 'Valencia', 'España', '23669988L', NULL, NULL, NULL, NULL, '2022-07-04 08:26:59', '2022-07-04 08:26:59'),
-(23, NULL, NULL, 'correo3@users.com', '$2a$07$aHasheaHachea052022$$u3MOxMbQ7xFIYWrouo18wPB80mWGne.S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTY5MjkxNjgsImV4cCI6MTY1NzAxNTU2OCwiZGF0YSI6eyJpZCI6IjIzIiwiZW1haWwiOiJjb3JyZW8zQHVzZXJzLmNvbSJ9fQ.vSXWRtEqF5dbFlu1hJ_swJ7MXEm0WYKU9vLqY8azq1Y', '1657015568', NULL, NULL, '2022-07-04 09:58:09', '2022-07-04 09:58:09'),
-(26, NULL, NULL, 'correo1@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTY5MjkwMTQsImV4cCI6MTY1NzAxNTQxNCwiZGF0YSI6eyJpZCI6IjI2IiwiZW1haWwiOiJjb3JyZW8xQGdtYWlsLmNvbSJ9fQ.f7DmLfHO5b2_ybXR5Fh6xvAjuX9qFiHWmnfPpT0qvA0', '1657015414', NULL, NULL, '2022-07-04 11:50:01', '2022-07-04 11:50:01');
+INSERT INTO `users` (`id_user`, `name_user`, `lastname_user`, `email_user`, `password_user`, `metodo_registro_user`, `phone_user`, `adress_user`, `cp_user`, `city_user`, `country_user`, `nif_user`, `token_user`, `token_exp_user`, `id_experience_user`, `publi_check_user`, `remember_me_user`, `id_education_user`, `date_create_user`, `date_update_user`) VALUES
+(85, 'Tarek', 'Zemmari Kissani', 'guirep@prueba.com', '$2a$07$aHasheaHachea052022$$u3MOxMbQ7xFIYWrouo18wPB80mWGne.S', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, '2022-07-08 16:00:25', '2022-07-08 16:00:25');
 
 --
 -- Índices para tablas volcadas
@@ -184,7 +177,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
